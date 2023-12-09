40e9a0da95466afd8f1e41aeeef0aaa692bcb89d
Fleberbr <eltondelira@gmail.com>
2023-12-09 17:29:42 -0300

    iniciando projeto com spring

---
+++ src/test/java/br/com/portfolio/elton/cadastrocliente/CadastroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portfolio.elton.cadastrocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
